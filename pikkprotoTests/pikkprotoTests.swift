507b591c16976917942c7e76278fc27bc7b0ca7a
Leo Nguyen <leo.t.beast@gmail.com>
2025-08-19 20:57:29 -0700

    Initial Commit

---
+++ pikkprotoTests/pikkprotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  pikkprotoTests.swift
+//  pikkprotoTests
+//
+//  Created by Leo Nguyen on 8/19/25.
+//
+
+import Testing
+@testable import pikkproto
+
+struct pikkprotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
